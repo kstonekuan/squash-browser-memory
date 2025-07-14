0d913455da5327d60e5af14bc3795c54c30d9b5a
Kingston <kingstonkuan@u.nus.edu>
2025-07-15 00:12:32 +0800

    add vite source map

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
 		outDir: "dist",
 		emptyOutDir: true,
 		modulePreload: false,
+		sourcemap: true,
 		rollupOptions: {
 			input: {
 				sidepanel: "sidepanel.html",
